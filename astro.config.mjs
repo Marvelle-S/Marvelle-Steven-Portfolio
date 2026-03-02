1b8dd4a85e0cdb9071676559f91e7f9eb3154060
MarvelleSteven(UG) <marvelle_s@outlook.com>
2026-03-02 03:14:21 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+  site: 'https://marvelle-s.github.io'
 
   vite: {
     plugins: [tailwindcss()],
