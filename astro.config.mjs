fa427125e52d3ccd2a5a82a7c59688574f6239d3
MarvelleSteven(UG) <marvelle_s@outlook.com>
2026-03-02 03:56:43 +0000

    Commit True

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
+// @ts-check
 import { defineConfig } from "astro/config";
+
 import tailwindcss from "@tailwindcss/vite";
+
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import vercel from "@astrojs/vercel";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc()],
+  integrations: [react(), markdoc(), keystatic()],
   site: 'https://marvelle-s.github.io',
   base: '/Marvelle-Steven-Portfolio',
-  output: "static",          // was "server"
+
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["@keystatic/core", "@keystatic/astro"],
+    },
   },
+
+  output: "server",
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
